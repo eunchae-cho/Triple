795d09ce3245207af0431a8051320246f78ba20b
eunchae-cho <dmsco8981@gmail.com>
2023-04-03 15:46:42 +0900

    create project

---
+++ demo/src/test/kotlin/com/triple/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.triple.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
